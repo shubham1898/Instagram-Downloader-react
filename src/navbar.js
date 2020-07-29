dd6d24e16cda9d482d172d3d034776062c4adbd6
Kumar Shubham <kumarshubham1898@gmail.com>
2020-07-29 18:15:43 +0530

    Update navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -47,7 +47,7 @@ class navbar extends Component {
                 
                 
                 
-                  <a class="nav-link nav-item active" onClick={this.toggleSearch} style={style1}>Search Username</a>
+                  <a class="navbar-brand active" onClick={this.toggleSearch} style={style1}>Search Username</a>
                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">  <span class="navbar-toggler-icon"></span>  </button>
 
               <div class="collapse navbar-collapse" id="navbarNav">
